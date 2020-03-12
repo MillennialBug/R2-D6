910edc9c563767c3164ea5a7389bb547141a055d
MillennialBug <17302342+MillennialBug@users.noreply.github.com>
2020-03-12 10:55:42 +0000

    Create colours.py

---
+++ colours.py
@@ -0,0 +1,13 @@
+from Discord import Colours
+
+colours = [Colour.teal(), Colour.dark_teal(), 
+           Colour.green(), Colour.dark_green(),
+           Colour.blue(), Colour.dark_blue(),
+           Colour.purple(), Colour.dark_purple(),
+           Colour.magenta(), Colour.dark_megenta(),
+           Colour.gold(), Colour.dark_gold(),
+           Colour.orange(), Colour.dark_orange(),
+           Colour.red(), Colour.dark_red(), 
+           Colour.lighter_grey(), Colour.light_grey(),
+           Colour.dark_grey(), Colour.darker_grey(),
+           Colour.blurple(), Colour.greyple()]
